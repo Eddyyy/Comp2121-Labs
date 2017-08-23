3b4831238acd7bfa7e801924e1bc83d222fc070c
Eddyyy <edward.thomson9@gmail.com>
2017-08-23 14:55:00 +1000

    worked on taskB

---
+++ Lab2/Lab2/TaskB.asm
@@ -0,0 +1,67 @@
+.include "m2560def.inc"
+; Lab2.asm
+;
+; Created: 23/08/2017 11:31:55 AM
+; Author : Edward Thomson, Zhiwei Cao
+;
+
+.equ n=10
+.equ x_const=3
+.def i=r8
+.def res_l=r16
+.def res_h=r17
+.def sum_0=r18
+.def sum_1=r19
+.def sum_2=r20
+.def tmp1=r9
+.def tmp2=r10
+
+
+.dseg
+.org 0x200
+A: .byte n
+
+.cseg
+initial:
+	ldi xl,low(A)
+	ldi xh,high(A)
+	clr i
+	clr res_l
+	clr res_h
+	clr sum_0
+	clr sum_1
+	clr sum_2
+
+main:
+
+forloop1:
+	cpi n, i
+	brlo forloop1_done
+	
+	st x, i
+	;get result from power
+
+	
+	ld r21, x+
+	mul r21,res_l
+	add sum_0,r0
+	adc sum_1,r1
+	mul r21,res_h
+	add sum_1,r0
+	adc sum_2,r1
+
+	inc i;
+	rjmp forloop1
+forloop1_done:
+
+end_main: rjmp end_main
+
+
+
+power:
+;-----------Prologue------------
+
+;-----------main Function-------
+
+;-----------Epilogue------------
+return:
