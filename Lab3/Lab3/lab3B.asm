db4e83c1dad087531739e22bc3812d8aee6bb9fa
ashx1900 <seahawkc@icloud.com>
2017-09-13 14:53:15 +1000

    Add files via upload

--- Lab3/Lab3/lab3B.asm
+++ Lab3/Lab3/lab3B.asm
@@ -3,8 +3,7 @@
 .def pattern =r17
 .def debounce = r18
 .def de2 = r19
-.def de3 = r20
-.def de4 = r21
+
 .cseg
 .org 0x0
 jmp RESET
@@ -26,8 +25,9 @@ clr temp
 out PORTC, temp			;activate
 out DDRD, temp
 out PORTD, temp
-ldi temp, (2 << ISC10) | (2 << ISC00)	;set INT0, 1 as falling-edge triggered interrupt
-sts EICRA, temp ; 
+ldi temp, (1 << ISC10) | (1 << ISC00)	;set INT0, 1 as falling-edge triggered interrupt
+sts EICRA, temp
+
 in temp, EIMSK	;enable INT0
 ori temp, (1<<INT0) | (1<<INT1)
 out EIMSK, temp
@@ -36,12 +36,9 @@ sei ;enable Global Interrupt
 jmp main
 
 EXT_INT0:
-;-------------prologue-------------
 push temp
 in temp, SREG
 push temp
-
-;-------------main-------------
 loop:
 	inc debounce
 	loopa:
@@ -58,8 +55,6 @@ dec pattern
 rjmp epilogue
 equal:
 	ldi pattern, 15	
-
-;--------epilogue-----------
 epilogue:
 mov temp, pattern
 out PORTC, temp
@@ -69,12 +64,9 @@ pop temp
 reti
 
 EXT_INT1:
-;-------------prologue-------------
 push temp
 in temp, SREG
 push temp
-
-;-------------main-------------
 loop2:
 	inc debounce
 	loopb:	;255*255/16MHz
@@ -85,7 +77,6 @@ loop2:
 	cpi debounce, 255
 	brlo loop2
 clr debounce
-
 cpi pattern, 15
 breq equal2
 inc pattern
@@ -93,7 +84,6 @@ rjmp epilogue2
 equal2:
 	ldi pattern, 0
 
-;--------epilogue-----------
 epilogue2:
 mov temp, pattern
 out PORTC, temp
