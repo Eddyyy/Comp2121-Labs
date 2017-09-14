0be605e051d622495c4854527c6e6cffa8fa60dc
Zhiwei Cao <jarvisczw@gmail.com>
2017-09-14 11:05:31 +1000

    Lab TaskA

--- Lab3/Lab3/lab3A.asm
+++ Lab3/Lab3/lab3A.asm
@@ -1,36 +1,38 @@
 .include "m2560def.inc"
 .def temp = r16
-.def debounce = r17
-.def de2 = r18
-
+.def flag0 = r17
+.def flag1 = r18
 .equ origin = 0x0F
 
 .cseg
 .org 0x0
-ldi temp, origin
-out PORTC, temp ; Write ones to all the LEDs
-out DDRC, temp ; PORTC is all outputs
-ser temp
-out PORTD, temp ; Enable pull-up resistors on PORTF
-clr temp
-out DDRD, temp ; PORTF is all inputs
+ldi flag0, 0
+ldi flag1, 0
 
-ldi temp, 15		;15 = 0b00001111
+ldi temp, origin
+out PORTC, temp 			; Write ones to all the LEDs
+out DDRC, temp 				; PORTC is all outputs
+ser temp
+out PORTD, temp 			; Enable pull-up resistors on PORTF
+clr temp
+out DDRD, temp 				; PORTF is all inputs
+
+ldi temp, 15				;15 = 0b00001111
 out PORTC, temp
 
+;-----------------------------------------
 switch0:
-sbic PIND, 0 ; Skip the next instruction if PB0 is pushed
-rjmp switch1 ; If not pushed, check the other switch
-loop:
-	inc debounce
-	loopa:
-		inc de2
-		cpi de2, 255	;255*255*1/16MHz= 0.004s
-		brlo loopa
-		clr de2
-	cpi debounce, 255
-	brlo loop
-clr debounce
+cpi PIND0, 0 				; I'm not sure, But I thinks that PIND0 indicate that the value on bit0 of PIND.
+breq instruction0			; If not pushed, check the other switch
+clr flag0
+rjmp switch1
+
+instruction0:
+cpi flag0, 1
+breq switch1
+
+ldi flag0, 1
+
 dec temp
 rjmp epilogue
 equal:
@@ -38,21 +40,19 @@ equal:
 epilogue:
 out PORTC, temp
 
+;------------------------------------------
 switch1:
-sbic PIND, 1 ; Skip the next instruction if PB1 is pushed
-rjmp switch0 ; If not pushed, check the other switch
+cpi PIND1, 0
+breq instruction1			; If not pushed, check the other switch
+clr flag1
+rjmp switch0
 
-loop2:
-	inc debounce
-	loopb:	;255*255/16MHz
-		inc de2
-		cpi de2, 255
-		brlo loopb
-		clr de2
-	cpi debounce, 255
-	brlo loop2
-clr debounce
 
+instruction1:
+cpi flag1, 1
+breq switch0
+
+ldi flag1, 1
 
 cpi temp, 15
 breq equal2
@@ -62,4 +62,5 @@ rjmp switch0
 equal2:
 	ldi temp, 0
 	out PORTC, temp
-	rjmp switch0 ; Now check PB0 again
+	rjmp switch0 			; Now check PB0 again
+
