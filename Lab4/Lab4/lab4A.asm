f5e34197e37a2d4e77b4175810a881275c119b2f
Eddyyy <edward.thomson9@gmail.com>
2017-09-20 15:18:23 +1000

    fix up lab4

--- Lab4/Lab4/lab4A.asm
+++ Lab4/Lab4/lab4A.asm
@@ -5,41 +5,10 @@
  *   Author: Edward
  */ 
 .include "m2560def.inc"
-.def a_l=r16
-.def a_h=r17
-.def b_l=r18
-.def b_h=r19
 
 .dseg
 
 .cseg
-
-ldi r16,low(1500)
-ldi r17,high(1500)
-ldi r18,low(3300)
-ldi r19,high(3300)
-
-
-while: 
-	cp a_l,b_l
-	cpc a_h,b_h
-	breq return
-if:	
-	cp a_l,b_l
-	cpc a_h,b_h
-	brlo else
-
-	sub a_l,b_l
-	sbc a_h,b_h
-
-	jmp endLoop
-else:
-	
-	sub b_l,a_l
-	sbc b_h,a_h
-
-endLoop:
-	rjmp while
 
 return:
 	rjmp return
